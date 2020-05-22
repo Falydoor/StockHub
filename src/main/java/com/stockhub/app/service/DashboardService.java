71ca309b95d1e176e5258621c5a441e6a80be9a0
Theo LEBRUN <mib.ftw@gmail.com>
2020-05-21 23:16:32 -0400

    Refactor on DashboardService

--- src/main/java/com/stockhub/app/service/DashboardService.java
+++ src/main/java/com/stockhub/app/service/DashboardService.java
@@ -3,12 +3,8 @@ package com.stockhub.app.service;
 import com.stockhub.app.domain.Stock;
 import com.stockhub.app.repository.StockRepository;
 import com.stockhub.app.service.dto.*;
-import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
-import org.apache.commons.math3.analysis.solvers.LaguerreSolver;
 import org.apache.commons.math3.fitting.PolynomialCurveFitter;
 import org.apache.commons.math3.fitting.WeightedObservedPoints;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -25,7 +21,9 @@ import static java.time.temporal.ChronoUnit.YEARS;
 
 @Service
 public class DashboardService {
-    private final Logger log = LoggerFactory.getLogger(DashboardService.class);
+    private final String DEFAULT_TICKER = "T";
+
+    private final double STRIKE_PERCENT = 0.05;
 
     private final YahooService yahooService;
 
@@ -37,8 +35,8 @@ public class DashboardService {
     }
 
     public List<ExpirationDTO> getExpirations() {
-        return yahooService.getExpirations("T")
-            .mapToObj(expiration -> new ExpirationDTO().expiration(expiration).expirationDays(ChronoUnit.DAYS.between(LocalDate.now(), toDate(expiration))))
+        return yahooService.getExpirations(DEFAULT_TICKER)
+            .mapToObj(this::getExpirationDTO)
             .sorted(Comparator.comparingLong(ExpirationDTO::getExpirationDays).reversed())
             .collect(Collectors.toList());
     }
@@ -68,23 +66,29 @@ public class DashboardService {
             .map(Stock::getTicker)
             .map(ticker -> getOption(ticker, expiration, yahooService.getQuote(ticker).getRegularMarketPrice(), yahooService.getLastYearDividends(ticker)))
             .filter(Objects::nonNull)
-            .map(option -> {
-                QuoteDTO quote = yahooService.getQuote(option.getTicker());
-
-                return new DashboardByExpirationDTO()
-                    .fiftyTwoWeekHigh(quote.getFiftyTwoWeekHigh())
-                    .fiftyTwoWeekLow(quote.getFiftyTwoWeekLow())
-                    .name(quote.getLongName())
-                    .price(quote.getRegularMarketPrice())
-                    .ticker(option.getTicker())
-                    .dividends(yahooService.getLastYearDividends(option.getTicker()))
-                    .calculatedPercentProfit(option.getCalculatedPercentProfit())
-                    .expiration(option.getExpiration())
-                    .expirationDays(option.getExpirationDays())
-                    .options(option.getOptions());
-            })
+            .map(this::getDashboardByExpiration)
             .sorted(Comparator.comparingDouble(DashboardByExpirationDTO::getCalculatedPercentProfit).reversed())
             .collect(Collectors.toList());
+    }
+
+    private DashboardByExpirationDTO getDashboardByExpiration(DashboardOptionsDTO option) {
+        QuoteDTO quote = yahooService.getQuote(option.getTicker());
+
+        return new DashboardByExpirationDTO()
+            .fiftyTwoWeekHigh(quote.getFiftyTwoWeekHigh())
+            .fiftyTwoWeekLow(quote.getFiftyTwoWeekLow())
+            .name(quote.getLongName())
+            .price(quote.getRegularMarketPrice())
+            .ticker(option.getTicker())
+            .dividends(yahooService.getLastYearDividends(option.getTicker()))
+            .calculatedPercentProfit(option.getCalculatedPercentProfit())
+            .expiration(option.getExpiration())
+            .expirationDays(option.getExpirationDays())
+            .options(option.getOptions());
+    }
+
+    private ExpirationDTO getExpirationDTO(long expiration) {
+        return new ExpirationDTO().expiration(expiration).expirationDays(ChronoUnit.DAYS.between(LocalDate.now(), toDate(expiration)));
     }
 
     private DashboardOptionsDTO getOption(String ticker, long expiration, double price, List<DivDTO> lastYearDividends) {
@@ -102,35 +106,33 @@ public class DashboardService {
             .expiration(expirationDate)
             .expirationDays(ChronoUnit.DAYS.between(LocalDate.now(), expirationDate))
             .options(options)
-            .calculatedPercentProfit(calcProfit(options, 0.05));
+            .calculatedPercentProfit(calcProfit(options));
     }
 
     private OptionWithProfitDTO buildDashboardOption(CallDTO call, double price, LocalDate expirationDate, List<DivDTO> lastYearDividends) {
-        double profitDiv = getDiv(lastYearDividends, expirationDate);
-
         return new OptionWithProfitDTO()
             .strike(call.getStrike())
-            .strikePercent(call.getStrike() / price - 1)
             .strikePrice(call.getLastPrice())
-            .profit(call.getStrike() - price)
-            .profitDiv(profitDiv)
+            .profitDiv(getProfitDivs(lastYearDividends, expirationDate))
             .price(price)
-            .profitTotal(call.getLastPrice() + (call.getStrike() - price) + profitDiv)
-            .profitPercent((call.getLastPrice() + call.getStrike() + profitDiv) / price - 1)
             .volume(call.getVolume())
             .openInterest(call.getOpenInterest());
     }
 
-    private double getDiv(List<DivDTO> lastYearDividends, LocalDate expirationDate) {
+    private double getProfitDivs(List<DivDTO> lastYearDividends, LocalDate expirationDate) {
         return LongStream.range(1, YEARS.between(LocalDate.now(), expirationDate) + 2)
-            .mapToDouble(i -> lastYearDividends.stream()
-                .filter(div -> div.getDate().plusYears(i).isAfter(LocalDate.now()) && div.getDate().plusYears(i).isBefore(expirationDate))
+            .mapToDouble(years -> lastYearDividends.stream()
+                .filter(div -> isValidProfit(div, years, expirationDate))
                 .mapToDouble(DivDTO::getDividends)
                 .sum())
             .sum();
     }
 
-    private double calcProfit(List<OptionWithProfitDTO> options, double strikePercent) {
+    private boolean isValidProfit(DivDTO div, long years, LocalDate expirationDate) {
+        return div.getDate().plusYears(years).isAfter(LocalDate.now()) && div.getDate().plusYears(years).isBefore(expirationDate);
+    }
+
+    private double calcProfit(List<OptionWithProfitDTO> options) {
         if (options.size() < 7) {
             return 0;
         }
@@ -141,24 +143,7 @@ public class DashboardService {
         PolynomialCurveFitter fitter = PolynomialCurveFitter.create(3);
         fitter.withStartPoint(new double[]{options.get(0).getStrikePercent(), options.get(0).getProfitPercent()});
         double[] coeffs = fitter.fit(obs.toList());
-        return coeffs[0] + coeffs[1] * strikePercent + coeffs[2] * Math.pow(strikePercent, 2);
-    }
-
-    private double solveTwentyPercent(List<OptionWithProfitDTO> options, long expiration) {
-        WeightedObservedPoints obs = new WeightedObservedPoints();
-        options.forEach(option -> obs.add(option.getVolume(), option.getStrikePercent(), option.getProfitPercent()));
-        PolynomialCurveFitter fitter = PolynomialCurveFitter.create(3);
-        fitter.withStartPoint(new double[]{options.get(0).getStrikePercent(), options.get(0).getProfitPercent()});
-        double[] coeffs = fitter.fit(obs.toList());
-        coeffs[0] -= 0.20;
-        PolynomialFunction polynomial = new PolynomialFunction(coeffs);
-        LaguerreSolver laguerreSolver = new LaguerreSolver();
-        try {
-            return laguerreSolver.solve(100, polynomial, options.get(0).getStrikePercent(), options.get(options.size() - 1).getStrikePercent());
-        } catch (Exception e) {
-            log.error("Unable to solve for {}", expiration);
-            return 0;
-        }
+        return coeffs[0] + coeffs[1] * STRIKE_PERCENT + coeffs[2] * Math.pow(STRIKE_PERCENT, 2);
     }
 
     private LocalDate toDate(long expiration) {
